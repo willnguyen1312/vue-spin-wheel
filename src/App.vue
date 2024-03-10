4db7c2e4915a7469c418a13b4f2b6de250a26c13
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-03-10 13:48:25 -0300

    Get go 💞

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const names = ["Vi", "Nam", "Truong", "Ha", "Hung", "Bach"];
+
+const intake = 5;
+
+const finalList = names.slice(0, intake);
+
+const getStyle = (index: number) => {
+  const rotate = 360 / intake;
+  const rotateDeg = rotate * index;
+  const sliceDegree = 180 - rotate;
+  return {
+    "--slice-degree": `${sliceDegree}deg`,
+    "--background-color": `hsl(${rotateDeg}, 100%, 50%)`,
+    "--rotate-degree": `${rotateDeg}deg`,
+    "--rotate-content": `${-rotateDeg}deg`,
+    "--text-position": "4%",
+    zIndex: index,
+  };
+};
+</script>
 
 <template>
   <div class="parent">
-    <div class="pie pie1">
-      <p class="content">Vi</p>
-    </div>
-    <div class="pie pie2">
-      <p class="content">Nam</p>
-    </div>
-    <div class="pie pie3">
-      <p class="content">Truong</p>
-    </div>
-    <div class="pie pie4">
-      <p class="content">Ha</p>
+    <div
+      v-for="(name, index) in finalList"
+      :key="name"
+      class="pie"
+      :style="getStyle(index)"
+    >
+      <div class="content">
+        <div class="text">
+          {{ name }}
+        </div>
+      </div>
     </div>
 
     <div class="center">🌀</div>
   </div>
 </template>
 
-<style scoped>
+<style>
 .center {
   position: absolute;
   width: 30px;
@@ -28,6 +48,7 @@
   display: grid;
   place-content: center;
   font-size: 23px;
+  z-index: 1000;
 
   top: 50%;
   left: 50%;
@@ -49,42 +70,17 @@
     content: "";
     border-radius: 300px 300px 0 0;
     transform-origin: 50% 100%;
-    transform: rotate(90deg);
+    rotate: var(--slice-degree);
+    background-color: var(--background-color);
   }
-}
 
-.pie1 {
-  &:before {
-    background-color: red;
-  }
-}
-
-.pie2 {
-  &:before {
-    background-color: green;
-  }
-  transform: rotate(90deg);
-}
-
-.pie3 {
-  transform: rotate(180deg);
-  &:before {
-    background-color: blue;
-  }
-}
-
-.pie4 {
-  transform: rotate(270deg);
-  &:before {
-    background-color: pink;
-  }
+  rotate: var(--rotate-degree);
 }
 
 .parent {
   width: 600px;
   height: 600px;
   position: relative;
-
   animation-duration: 10s;
   animation-fill-mode: forwards;
   animation-name: spinning;
@@ -101,14 +97,20 @@
 }
 
 .content {
-  display: grid;
-  place-content: center;
-  z-index: 999;
-  width: 50%;
-  height: 100%;
-  position: absolute;
-  rotate: -45deg;
   font-size: xx-large;
   font-weight: bold;
+
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  border-radius: 300px 300px 0 0;
+  transform-origin: 50% 100%;
+  rotate: var(--slice-degree);
+}
+
+.text {
+  position: absolute;
+  left: var(--text-position);
+  bottom: var(--text-position);
 }
 </style>

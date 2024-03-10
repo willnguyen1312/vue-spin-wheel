2dbd6523ed3d7128e4a55b0efc87c4e495c83d6e
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-03-10 14:41:05 -0300

    Awesome spin wheel 🌀

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-const names = ["Vi", "Nam", "Truong", "Ha", "Hung", "Bach"];
+import { ref } from "vue";
+const state = ref<"idle" | "spinning" | "finished">("idle");
+const spinDeg = ref(0);
+const currentDeg = ref(0);
+const spinRef = ref<HTMLElement>();
+const names = ["Vi", "Nam", "Truong", "Ha", "Dung", "Bach"];
 
-const intake = 5;
+const intake = 6;
 
 const finalList = names.slice(0, intake);
 
@@ -18,29 +23,119 @@ const getStyle = (index: number) => {
     zIndex: index,
   };
 };
+
+const handleAnimationEnd = () => {
+  state.value = "finished";
+  spinRef.value?.classList.remove("spin-animation");
+  if (spinRef.value) {
+    currentDeg.value = spinDeg.value % 360;
+    spinDeg.value = currentDeg.value;
+  }
+};
+
+const handleClick = () => {
+  if (state.value === "idle" || state.value === "finished") {
+    state.value = "spinning";
+    spinDeg.value += 7000 + Math.floor(Math.random() * 5000);
+    spinRef.value?.classList.add("spin-animation");
+  }
+};
 </script>
 
 <template>
-  <div class="parent">
-    <div
-      v-for="(name, index) in finalList"
-      :key="name"
-      class="pie"
-      :style="getStyle(index)"
-    >
-      <div class="content">
-        <div class="text">
-          {{ name }}
+  <div class="wrapper">
+    <div class="spin-wrapper">
+      <div ref="spinRef" class="spin" @animationend="handleAnimationEnd">
+        <div
+          v-for="(name, index) in finalList"
+          :key="name"
+          class="pie"
+          :style="getStyle(index)"
+        >
+          <div class="content">
+            <div class="text">
+              {{ name }}
+            </div>
+          </div>
         </div>
+
+        <button
+          :disabled="state === 'spinning'"
+          @click="handleClick"
+          class="activator"
+        >
+          🌀
+        </button>
+      </div>
+
+      <div class="pointer-wrapper">
+        <div class="pointer"></div>
       </div>
     </div>
-
-    <div class="center">🌀</div>
   </div>
 </template>
 
 <style>
-.center {
+.wrapper {
+  --pointer-height: 40px;
+  --spin-wrapper-size: 600px;
+  --spinning-deg: v-bind(spinDeg + "deg");
+  display: flex;
+  gap: 20px;
+}
+
+.spin-wrapper {
+  width: var(--spin-wrapper-size);
+  height: var(--spin-wrapper-size);
+}
+
+.spin {
+  rotate: v-bind(currentDeg + "deg");
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  animation-duration: 10s;
+  animation-fill-mode: forwards;
+  animation-timing-function: cubic-bezier(0.44, -0.205, 0, 1.13);
+}
+
+.spin-animation {
+  animation-name: spinning;
+}
+
+.pointer-wrapper {
+  height: var(--pointer-height);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+}
+
+.pointer {
+  top: -100%;
+  height: 40px;
+  width: 80px;
+  overflow: hidden;
+  position: absolute;
+  transform-origin: 50% 100%;
+  rotate: 120deg;
+
+  &:before {
+    height: inherit;
+    width: inherit;
+    position: absolute;
+    content: "";
+    border-radius: 40px 40px 0 0;
+    transform-origin: 50% 100%;
+    rotate: 120deg;
+    background-color: blueviolet;
+  }
+}
+
+.activator {
+  border-color: transparent;
+  cursor: pointer;
   position: absolute;
   width: 30px;
   height: 30px;
@@ -57,8 +152,8 @@ const getStyle = (index: number) => {
 }
 
 .pie {
-  height: 300px;
-  width: 600px;
+  height: calc(var(--spin-wrapper-size) / 2);
+  width: var(--spin-wrapper-size);
   overflow: hidden;
   position: absolute;
   transform-origin: 50% 100%;
@@ -68,7 +163,8 @@ const getStyle = (index: number) => {
     width: inherit;
     position: absolute;
     content: "";
-    border-radius: 300px 300px 0 0;
+    border-radius: calc(var(--spin-wrapper-size) / 2)
+      calc(var(--spin-wrapper-size) / 2) 0 0;
     transform-origin: 50% 100%;
     rotate: var(--slice-degree);
     background-color: var(--background-color);
@@ -77,29 +173,18 @@ const getStyle = (index: number) => {
   rotate: var(--rotate-degree);
 }
 
-.parent {
-  width: 600px;
-  height: 600px;
-  position: relative;
-  animation-duration: 10s;
-  animation-fill-mode: forwards;
-  animation-name: spinning;
-  animation-timing-function: cubic-bezier(0.44, -0.205, 0, 1.13);
-}
-
 @keyframes spinning {
   from {
-    transform: rotate(0);
+    rotate: v-bind(currentDeg + "deg");
   }
   to {
-    transform: rotate(10000deg);
+    rotate: var(--spinning-deg);
   }
 }
 
 .content {
   font-size: xx-large;
   font-weight: bold;
-
   height: 100%;
   width: 100%;
   position: absolute;
